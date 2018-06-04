e6728d977d206c33e9d9347ea9cf7567f8d2c591
ehoxha15 <37331414+ehoxha15@users.noreply.github.com>
2018-06-04 21:48:30 +0200

    Delete delete_notif.php

--- coding/application/delete_notif.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require "crudAdmin.php";
-if(isset($_GET['id'])){
-    $crd= new \core\people\crudAdmin();
-    $crd->deleteNotification($_GET['id']);
-    header('Location:admin.php');
-}
