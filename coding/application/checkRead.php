08e738f857f26aaf2693ade94818534fa0b23d6b
ehoxha15 <37331414+ehoxha15@users.noreply.github.com>
2018-06-04 21:44:11 +0200

    Delete checkRead.php

--- coding/application/checkRead.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'session.php';
-require_once 'crudAccountant.php';
-
-$acc = new \core\people\crudAccountant();
-
-$result=$acc->readNotif($_POST['id']);
-if($result) header("Location:viewNotifications.php");
-?>
